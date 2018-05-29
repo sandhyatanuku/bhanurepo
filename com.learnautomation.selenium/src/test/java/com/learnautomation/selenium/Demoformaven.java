e9c8f9c779a39db11ce08f42d3b5a943ee3235f0
sandhya <sandh@DESKTOP-CVP41T7.home>
2018-05-29 15:09:01 -0400

    sandhya<sandh@DESKTOP-CVP41T7.home>
    Change-Id: I3cc1b81be3dbeb974ee3acca3724285e304e3106

---
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/Demoformaven.java
@@ -0,0 +1,11 @@
+package com.learnautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class Demoformaven {
+	@Test
+	
+	public void testhello() {
+System.out.println("hello worldf");
+}
+}
